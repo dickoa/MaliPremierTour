f56b4c4690fdcfdb9489159d38ffdc326d742f04
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2013-08-07 13:43:29 +0000

    Small edit

--- ui.R
+++ ui.R
@@ -10,11 +10,16 @@ shinyUI(pageWithSidebar(
     sidebarPanel(
         selectInput("echelle", "Echelle : ",
                 choices = c("Region", "Cercle")),
-        helpText("\nSimple visualisation des résultats provisoires du premier tour",
-                 "des  élections présidentielle du 28 juillet au Mali.\n",
+        helpText("Simple visualisation des résultats provisoires du premier tour",
+                 "des  élections présidentielle du 28 juillet au Mali.",
+                  HTML("<br></br>"),
                  "Ces résultats sont disponibles par région et par cercle.",
                  HTML("<br></br> Source : <a href = \"http://www.matcl.gov.ml/\">Ministère de l’administration territoriale, de la décentralisation de l'aménagement du territoire</a>"),
-                 HTML("<br></br>Code source disponible sur <a href = \"https://github.com/dickoa/OpenDataApp\">Github</a>"))
+
+                 HTML('<br></br><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Mali 2013 : premier tour</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Ahmadou H. Dicko</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.'),
+                  HTML('<br></br><img src="jokkolabs_logo.png" alt="File Type">'),
+                 HTML("<br></br>Code source disponible sur <a href = \"https://github.com/dickoa/OpenDataApp\">Github</a>")
+                 )
         ),
     mainPanel(
       tabsetPanel(
